15f06f7b4e22f0612346d41c1d8ae0d61542857e
Ilya Gornev <i.a.gornev@gmail.com>
2022-03-22 14:19:13 +0700

    Separate file for single user proxy

---
+++ SingleUserProxy.py
@@ -0,0 +1,82 @@
+#import socket
+from queue import Queue, Empty
+from select import select
+
+class SingleUserProxy:
+    def __init__(self, conn1, conn2):
+        self._socket1 = conn1[0]
+        self._socket2 = conn2[0]
+        
+        self._addresses = {}
+        self._addresses[self._socket1] = conn1[1]
+        self._addresses[self._socket2] = conn2[1]
+        
+        self._dataToSend = {}
+        self._dataToSend[self._socket1] = Queue()
+        self._dataToSend[self._socket2] = Queue()
+        
+        self._socketsToRead = [ self._socket1, self._socket2 ]
+        self._socketsToWrite = [ self._socket1, self._socket2 ]
+    
+    def _closeConnection(self, socket):
+        if not (socket is self._socket1 or socket is self._socket2):
+            raise ValueError("Incorrect socket value")
+        
+        socket.close()
+        self._socketsToRead.clear()
+        self._socketsToWrite.remove(socket)
+        
+        print(f'Closed connection with {self._addresses[socket]}')
+        if not self._socketsToWrite:
+            return False
+        else:
+            return True
+    
+    def _handleMessage(self, fromSocket, toSocket):
+        if not (fromSocket is self._socket1 and toSocket is self._socket2 or fromSocket is self._socket2 and toSocket is self._socket1):
+            raise ValueError("Incorrect sockets' values")
+        
+        message = fromSocket.recv(1024)
+        self._dataToSend[toSocket].put(message)
+        
+        print(f'Message recieved from {self._addresses[fromSocket]}: {message}')
+        if not message:
+            return False
+        else:
+            return True
+    
+    def _sendMessageFromQueue(self, toSocket):
+        if not (toSocket is self._socket1 or toSocket is self._socket2):
+            raise ValueError("Incorrect socket value")
+        
+        try:
+            message = self._dataToSend[toSocket].get(block=False)
+            print(f'Message sent to {self._addresses[toSocket]}: {message}')
+            if not message:
+                return self._closeConnection(toSocket)
+            else:
+                toSocket.sendall(message)
+                return True
+        except Empty:
+            return True
+    
+    def serveForever(self):
+        socket1Alive = True
+        socket2Alive = True
+        while socket1Alive or socket2Alive:
+            readyToRead, readyToWrite, inError = select(self._socketsToRead, self._socketsToWrite, [], 60)
+            
+            for toSocket in readyToWrite:
+                keepWorking = self._sendMessageFromQueue(toSocket)
+                if not keepWorking:
+                    return
+            
+            for fromSocket in readyToRead:
+                if fromSocket is self._socket1:
+                    connAlive = self._handleMessage(self._socket1, self._socket2)
+                else:
+                    connAlive = self._handleMessage(self._socket2, self._socket1)
+                if not connAlive:
+                    keepWorking = self._closeConnection(fromSocket)
+                    if not keepWorking:
+                        return
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from argparse import ArgumentParser
 import socket
-import select
+from SingleUserProxy import SingleUserProxy
 
 parser = ArgumentParser(description='Echo server.')
 parser.add_argument('-a', '--address', help='host ip address', default='localhost')
@@ -10,51 +10,12 @@ args = parser.parse_args()
 HOST = args.address
 PORT = int(args.port)
 
-TARGET_PORT = 8080
-
-# Класс-помощник для планирования и выполнения отправки сообщений
-class WriteTaskPlanner():
-    _writeTasks = {}  # ключ - сокет, значение - массив данных, которые надо отправить
-    
-    def GetSocketsExpectingWriting(self):
-        return self._writeTasks.keys()
-    
-    def CreateWriteTask(self, socketToWrite, data):
-        if self._writeTasks.get(socketToWrite) is None:
-            self._writeTasks[socketToWrite] = [data]
-        else:
-            self._writeTasks[socketToWrite].append(data)
-        print(f'Created task: {data}')
-    
-    def PerformAllTasks(self, socket):
-        for data in self._writeTasks[socket]:
-            if not data:
-                socket.close()
-                print('Connection closed')
-            else:
-                socket.sendall(data)
-                print(f'Data sent: {data}')
-        self._writeTasks.pop(socket)  # удалим выполненную задачу
-        print('Deleted task')
-
-# Создаёт задачу по пересылке сообщения (1024 байта) из fromSocket в toSocket.
-# Возвращает true, если соединение ещё не закрыто, иначе false.
-def HandleMessage(fromSocket, toSocket, planner):
-    clientMessage = fromSocket.recv(1024)
-    print(f'Data recieved: {clientMessage}')
-    planner.CreateWriteTask(toSocket, clientMessage)
-    if not clientMessage:
-        return False
-    else:
-        return True
+TARGET_ADDR = ('localhost', 8080)
 
 hostSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 hostSocket.bind((HOST, PORT))
 hostSocket.listen(1)
 print(f'Listening to {HOST}:{PORT}');
-
-planner = WriteTaskPlanner()
-interlocutors = {}
 
 try:
     while True:
@@ -62,23 +23,10 @@ try:
         print(f'New client: {addr}')
         
         serverSocket = socket.socket()
-        serverSocket.connect(('localhost', TARGET_PORT))
-        interlocutors[clientSocket] = serverSocket
-        interlocutors[serverSocket] = clientSocket
-        print(f'Connected {addr} with localhost:{TARGET_PORT}')
+        serverSocket.connect(TARGET_ADDR)
+        proxy = SingleUserProxy((clientSocket, addr), (serverSocket, TARGET_ADDR))
+        print(f'Connected {addr} with {TARGET_ADDR}')
         
-        connAlive = True
-        while connAlive:
-            readyToRead, readyToWrite, inError = select.select([clientSocket, serverSocket], planner.GetSocketsExpectingWriting(), [], 60)
-            for s in readyToWrite:
-                planner.PerformAllTasks(s)
-            for fromSocket in readyToRead:
-                toSocket = interlocutors[fromSocket]
-                connAlive = HandleMessage(fromSocket, toSocket, planner)
-                if not connAlive:
-                    print(f'Connection closed: {addr}')
-                    fromSocket.close()
-                    interlocutors.pop(fromSocket)
-                    interlocutors.pop(toSocket)
+        proxy.serveForever()
 except KeyboardInterrupt:
     pass
