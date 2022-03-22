5b946816254af28ecba25d6bbea4183db48f02d0
Ilya Gornev <i.a.gornev@gmail.com>
2022-03-22 11:12:04 +0700

    Address from arguments, more output

--- server.py
+++ server.py
@@ -1,18 +1,27 @@
 import socket
+from argparse import ArgumentParser
 
-HOST = 'localhost'
-PORT = 3000
+parser = ArgumentParser(description='Echo server.')
+
+parser.add_argument('-a', '--address', help='host ip address', default='localhost')
+parser.add_argument('-p', '--port', help='port to listen', default='3000')
+
+args = parser.parse_args()
+HOST = args.address
+PORT = int(args.port)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
+print(f'Listening to {HOST}:{PORT}');
 
 conn, addr = s.accept()
 print(f'Connected by {addr}')
 
 while 1:
     data = conn.recv(1024)
-    if not data: 
+    print(f'Recieved data: {data}')
+    if not data:
         break
     conn.sendall(data)
 
