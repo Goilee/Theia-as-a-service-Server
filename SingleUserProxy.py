a65e59feeb2ac7c8cd2f93e2bec625f6b7119514
Ilya Gornev <i.a.gornev@gmail.com>
2022-04-18 09:30:23 +0700

    Dont send 'conn closed' to client

--- SingleUserProxy.py
+++ SingleUserProxy.py
@@ -1,91 +1,101 @@
 #import socket
 from queue import Queue, Empty
 from select import select
+import socket
 
 class SingleUserProxy:
     PACKET_SIZE = 1024
 
-    def __init__(self, conn1, conn2):
-        self._socket1 = conn1[0]
-        self._socket2 = conn2[0]
+    def __init__(self, clientConn, serverConn):
+        self._clientSock = clientConn[0]
+        self._serverSock = serverConn[0]
         
         self._addresses = {}
-        self._addresses[self._socket1] = conn1[1]
-        self._addresses[self._socket2] = conn2[1]
+        self._addresses[self._clientSock] = clientConn[1]
+        self._addresses[self._serverSock] = serverConn[1]
         
         self._dataToSend = {}
-        self._dataToSend[self._socket1] = Queue()
-        self._dataToSend[self._socket2] = Queue()
+        self._dataToSend[self._clientSock] = Queue()
+        self._dataToSend[self._serverSock] = Queue()
         
-        self._socketsToRead = [ self._socket1, self._socket2 ]
-        self._socketsToWrite = [ self._socket1, self._socket2 ]
+        self._socketsToRead = [ self._clientSock, self._serverSock ]
+        self._socketsToWrite = [ self._clientSock, self._serverSock ]
     
-    def _closeConnection(self, socket):
-        if not (socket is self._socket1 or socket is self._socket2):
-            raise ValueError("Incorrect socket value")
-        
-        socket.close()
-        self._socketsToRead.clear()
-        self._socketsToWrite.remove(socket)
-        
-        print(f'Closed connection with {self._addresses[socket]}')
-        return False if not self._socketsToWrite else True
+    def _closeConnection(self):
+        self._clientSock.close()
+        self._serverSock.close()
+        print(f'Closed connection with {self._addresses[self._clientSock]}')
     
     def _handleMessage(self, fromSocket):
-        if not (fromSocket is self._socket1 or fromSocket is self._socket2):
+        if not (fromSocket is self._clientSock or fromSocket is self._serverSock):
             raise ValueError("Incorrect socket value")
         
-        toSocket = self._socket2 if fromSocket is self._socket1 else self._socket1
+        toSocket = self._serverSock if fromSocket is self._clientSock else self._clientSock
         
         message = b''
-        connClosed = False
         try:
-            while True:
-                message += fromSocket.recv(self.PACKET_SIZE)
-                
-                length = len(message)
-                if length == 0:
-                    connClosed = True
-                if length < self.PACKET_SIZE:
-                    break
+            chunk = b'1'  # чтобы цикл выполнился минимум один раз
+            while chunk:
+                chunk = fromSocket.recv(self.PACKET_SIZE)
+                message += chunk
         except BlockingIOError:
             pass
         print(f'Message recieved from {self._addresses[fromSocket]}: {len(message)}')
-        if toSocket in self._dataToSend.keys():
+        connClosed = not message
+        if not connClosed and toSocket in self._dataToSend.keys():
             self._dataToSend[toSocket].put(message)
         return not connClosed
     
     def _sendMessageFromQueue(self, toSocket):
-        if not (toSocket is self._socket1 or toSocket is self._socket2):
+        if not (toSocket is self._clientSock or toSocket is self._serverSock):
             raise ValueError("Incorrect socket value")
         
         try:
             message = self._dataToSend[toSocket].get(block=False)
+            toSocket.sendall(message)
             print(f'Message sent to {self._addresses[toSocket]}: {len(message)}')
-            if not message:
-                return self._closeConnection(toSocket)
-            else:
-                toSocket.sendall(message)
-                return True
         except Empty:
-            return True
+            print(f'Tried to send to {self._addresses[toSocket]}, but no message queued')
+    
+    def _serverReconnect(self):
+        addr = self._addresses.pop(self._serverSock)
+        dataToSend = self._dataToSend.pop(self._serverSock)
+        self._socketsToRead.remove(self._serverSock)
+        self._socketsToWrite.remove(self._serverSock)
+        
+        self._serverSock.close()
+        self._serverSock = socket.socket()
+        self._serverSock.connect(addr)
+        print(f'Reconnected to {addr}')
+        
+        self._addresses[self._serverSock] = addr
+        self._dataToSend[self._serverSock] = dataToSend
+        self._socketsToRead.append(self._serverSock)
+        self._socketsToWrite.append(self._serverSock)
     
     def serveForever(self):
-        self._socket1.setblocking(False)
-        self._socket2.setblocking(False)
-        socket1Alive = True
-        socket2Alive = True
-        while socket1Alive or socket2Alive:
-            readyToRead, readyToWrite, inError = select(self._socketsToRead, self._socketsToWrite, [], 60)
+        self._clientSock.setblocking(False)
+        self._serverSock.setblocking(False)
+        while True:
+            print('Waiting to read...')
+            readyToRead, readyToWrite, inError = select(self._socketsToRead, [], [], 5)
+            print(f'{len(readyToRead)} ready to read')
             
             for fromSocket in readyToRead:
                 connAlive = self._handleMessage(fromSocket)
                 if not connAlive:
-                    keepWorking = self._closeConnection(fromSocket)
-                    if not keepWorking:
+                    if fromSocket is self._clientSock:
+                        self._closeConnection()
                         return
+                    else:
+                        self._serverReconnect()
             
-            for toSocket in readyToWrite:
-                keepWorking = self._sendMessageFromQueue(toSocket)
-                if not keepWorking:
-                    return
+            socketsToWrite = list(filter(lambda s: not self._dataToSend[s].empty(), self._socketsToWrite))
+            if len(socketsToWrite) > 0:
+                print('Waiting to write...')
+                readyToRead, readyToWrite, inError = select([], socketsToWrite, [], 5)
+                print(f'{len(readyToWrite)} ready to write')
+            
+                for toSocket in readyToWrite:
+                    if toSocket:
+                        self._sendMessageFromQueue(toSocket)
