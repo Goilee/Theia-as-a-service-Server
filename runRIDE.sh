c02e2175960c06794857f9b128644a22cb4ff4bc
Ilya Gornev <i.a.gornev@gmail.com>
2022-04-25 12:10:45 +0700

    Rewrote everything with Flask

--- runRIDE.sh
+++
@@ -1,4 +0,0 @@
-# Запускает контейнер RIDE на случайном порту и возвращает выделенный порт и ID контейнера
-ID=$(docker run -dp 3000 ride)
-docker inspect -f '{{ (index (index .NetworkSettings.Ports "3000/tcp") 0).HostPort }}' $ID
-echo $ID
--- server.py
+++ server.py
@@ -1,46 +1,39 @@
 from argparse import ArgumentParser
-import subprocess
-from http.server import HTTPServer, BaseHTTPRequestHandler
-import time
+from subprocess import Popen, PIPE
+from flask import Flask, Blueprint, render_template, request, session, abort, current_app
+
+
+parser = ArgumentParser(description='RIDE server.')
+parser.add_argument('-a', '--address', dest='HOST', help='host ip address', default='localhost')
+parser.add_argument('-p', '--port', dest='PORT', help='port to listen', type=int, default=3000)
+parser.add_argument('-d', '--debug', dest='DEBUG', help='run in debug mode', action='store_const', const=True, default=False)
+args = parser.parse_args()
+
 
 # запускает команду в shell и возвращает вывод
 def run_cmd(cmd):
-    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+    process = Popen(cmd, stdout=PIPE, shell=True)
     return process.communicate()[0].decode('utf-8')
 
-# запускает контейнер RIDE на случайном порту и возвращает кортеж из ID и выделенного порта
-def run_RIDE():
-    container = run_cmd('docker run -dp 3000 ride')[:-1]
-    port = run_cmd("docker inspect -f '{{ (index (index .NetworkSettings.Ports \"3000/tcp\") 0).HostPort }}' " + container)
-    print(f'Started RIDE container (id={container}) on port {port}')
-    return (container, port)
 
-# возвращает, готов ли контейнер принимает соединения
-def is_RIDE_ready(container):
-    result = run_cmd(f'docker logs {container} | grep "Theia app listening"')
-    return 'Error' not in result and len(result) > 0
+# запускает контейнер RIDE на случайном порту и возвращает кортеж (ip, port)
+def run_container():    
+    host = args.HOST
+    container = run_cmd(f'docker run --ip={host} --detach --publish 3000 ride')[:-1]
+    port = int(run_cmd("docker inspect -f '{{ (index (index .NetworkSettings.Ports \"3000/tcp\") 0).HostPort }}' " + container))
+    print(f'Started RIDE container (id={container}) on ({host},{port})')
+    return (host, port)
 
-class Redirect(BaseHTTPRequestHandler):
-    def do_GET(self):
-        (container, port) = run_RIDE()
-        while not is_RIDE_ready(container):
-            time.sleep(0.5)
-        self.send_response(302)
-        self.send_header('Location', f'http://{HOST}:{port}/')
-        self.end_headers()
 
-parser = ArgumentParser(description='RIDE server.')
-parser.add_argument('-a', '--address', help='host ip address', default='localhost')
-parser.add_argument('-p', '--port', help='port to listen', default='3000')
+main = Blueprint('main', __name__)
 
-args = parser.parse_args()
-HOST = args.address
-PORT = int(args.port)
+@main.route('/')
+def index():
+    (host, port) = run_container()
+    return render_template('loader.html'), { "Refresh": f"10; url=http://{host}:{port}" }
 
-server = HTTPServer((HOST, PORT), Redirect)
-print(f'Listening on {HOST}:{PORT}...')
-try:
-    server.serve_forever()
-except KeyboardInterrupt:
-    print("Stopping the server...")
-    server.server_close()
+
+app = Flask(__name__)
+app.register_blueprint(main)
+app.run(host = args.HOST, port = args.PORT, debug = args.DEBUG)
+
