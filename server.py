b1b2ae41aa3cd9d6daede08d52bf0d2ff117388c
Ilya Gornev <i.a.gornev@gmail.com>
2022-04-25 12:58:34 +0700

    Scheduler deleting containers
    Co-authored-by: wbondarchuk <wbondarchuk@gmail.com>

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from argparse import ArgumentParser
 from subprocess import Popen, PIPE
-from flask import Flask, Blueprint, render_template, request, session, abort, current_app
+from flask import Flask, Blueprint, render_template
+from apscheduler.schedulers.blocking import BlockingScheduler
+from threading import Thread
+from os import _exit
 
 
 parser = ArgumentParser(description='RIDE server.')
@@ -25,6 +28,40 @@ def run_container():
     return (host, port)
 
 
+# удаляет контейнер и возвращает вывод команды
+def force_remove_container(id):
+    cmd = f'docker rm -f {id}'
+    result = run_cmd(f'docker rm -f {id}')[:-1]
+    print(f'Force removed: {result}')
+    return result
+
+
+# возвращает номер последней строки, содержащей подстроку line (-1 при отсутствии)
+def find_last_line_in_logs(container, substr):
+    result = run_cmd(f'''docker logs {container} 2>&1 | grep -n "{substr}" | tail --lines=1''')
+    try:
+        lineNumber = int(result[0:result.index(':')])
+        return lineNumber
+    except ValueError:
+        return -1
+
+
+# возвращает список айднишников запущенных контейнеров
+def get_running_containers():
+    result = run_cmd('docker ps -q --filter "ancestor=ride"')
+    return result.splitlines()
+
+
+# удаляет запущенные контейнеры, из которых вышел юзер (или все, если параметр True)
+def clean_containers(cleanAll = False):
+    for container in get_running_containers():
+        clientEnter = find_last_line_in_logs(container, "Set client")
+        clientExit = find_last_line_in_logs(container, "All contributions have been stopped")
+        print (f'Container {container}: entered {clientEnter}, exited {clientExit}')
+        if clientExit > clientEnter or cleanAll:
+            force_remove_container(container)
+
+
 main = Blueprint('main', __name__)
 
 @main.route('/')
@@ -33,7 +70,17 @@ def index():
     return render_template('loader.html'), { "Refresh": f"10; url=http://{host}:{port}" }
 
 
-app = Flask(__name__)
-app.register_blueprint(main)
-app.run(host = args.HOST, port = args.PORT, debug = args.DEBUG)
+INTERVAL_IN_SECONDS = 300
+scheduler = BlockingScheduler()
+scheduler.add_job(clean_containers, 'interval', seconds=INTERVAL_IN_SECONDS)
+cleaner_thread = Thread(target=scheduler.start, args=())
+cleaner_thread.start()
+
+try:
+    app = Flask(__name__)
+    app.register_blueprint(main)
+    app.run(host = args.HOST, port = args.PORT, debug = args.DEBUG)
+finally:
+    clean_containers(True)
+    _exit(0)
 
