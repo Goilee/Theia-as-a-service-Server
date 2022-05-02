624585b3d5d1cecb3ae675f7cbd87b7545baedf0
Ilya Gornev <i.a.gornev@gmail.com>
2022-05-02 14:26:29 +0700

    Use config.ini

--- run_project.py
+++ run_project.py
@@ -1,31 +1,30 @@
-import os
-
-from project import app
+from argparse import ArgumentParser
+from configparser import ConfigParser
 from apscheduler.schedulers.blocking import BlockingScheduler
 from threading import Thread
+from os import _exit
+
+from project import app
 from project.docker_manager import *
 
-# TODO: включить когда будет готово
+SETTINGS_FILE = 'config.ini'
 
-# SETTINGS_FILE = 'config.ini'
-#
-# parser = ArgumentParser(description=f'Docker application server. Look for parameters in the {SETTINGS_FILE}.')
-# parser.add_argument('-d', '--debug', help='run in debug mode', action='store_const', const=True, default=False)
-# args = parser.parse_args()
-#
-# config = ConfigParser()
-# config.read(SETTINGS_FILE)
+parser = ArgumentParser(description=f'Docker application server. Look for parameters in the {SETTINGS_FILE}.')
+parser.add_argument('-d', '--debug', help='run in debug mode', action='store_const', const=True, default=False)
+args = parser.parse_args()
 
+config = ConfigParser()
+config.read(SETTINGS_FILE)
 
-INTERVAL_IN_SECONDS = 300
 scheduler = BlockingScheduler()
-scheduler.add_job(clean_containers, 'interval', seconds=INTERVAL_IN_SECONDS)
+scheduler.add_job(clean_containers, 'interval', seconds=int(config['CLEANER']['Time_interval_in_seconds']))
 cleaner_thread = Thread(target=scheduler.start, args=())
 cleaner_thread.start()
 
 with app.app_context():
     try:
-        app.run(host='127.0.0.1', port='5000', debug=True)
+        app.run(host=config['NETWORK']['Host'], port=config['NETWORK']['Port'], debug=args.debug)
     finally:
         clean_containers(True)
-        os._exit(0)
+        _exit(0)
+
