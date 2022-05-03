0b86b16dadc484b227bdc7ae859ac26c7ba5a938
Goilee <i.a.gornev@gmail.com>
2022-05-03 16:04:53 +0700

    Auto create db

--- db_creator.py
+++
@@ -1,5 +0,0 @@
-from project import db, app
-
-
-with app.app_context():
-    db.create_all(app=app)
--- project/__init__.py
+++ project/__init__.py
@@ -1,6 +1,7 @@
 # init.py
 
-import os.path
+from os.path import exists, join
+from pathlib import Path
 from os import environ
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -23,30 +24,34 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SQLALCHEMY_ECHO'] = environ.get('SQLALCHEMY_ECHO') in ('1', 'True')
-
+    
     db.init_app(app)
-
+    
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
-
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
+    
+    environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    
     from .models import Users
-
+    
     @login_manager.user_loader
     def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
         return Users.query.get(int(user_id))
-
+    
     # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
-
+    
     # blueprint for non-auth parts of app
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
+    
+    if not exists(join(Path(__file__).parent, "db.sqlite")):
+        app.logger.info('Database not found, creating new database...')
+        db.create_all(app=app)
+    
     return app
 
 
